2d14c1cb97e5f4fcb189d8a4a8d94c9e26cd925a
worace <horace.d.williams@gmail.com>
2015-02-23 14:01:31 -0700

    add a root-level mastermind file which can serve as entry into the program

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,14 +1,18 @@
 require './lib/mastermind'
 
-puts "Welcome to Mastermind"
+class Runner
+  def run
+    puts "Welcome to Mastermind"
 
-mastermind = Mastermind.new
-response = nil
+    mastermind = Mastermind.new
+    response = nil
 
-until response && response.status == :won
-  print "> "
-  input = gets.chomp
-  response = mastermind.execute(input)
-  puts response.message
+    until response && response.status == :won
+      print "> "
+      input = gets.chomp
+      response = mastermind.execute(input)
+      puts response.message
+    end
+    puts "Goodbye!"
+  end
 end
-puts "Goodbye!"
